9b9feabc9d0346ee3bd8fc489b4c5565e5940663
Ashwin Kumar Udayakumar <ashwinkumar14102000@gmail.com>
2022-12-09 17:40:09 -0500

    Added node server

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -9,6 +9,15 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post("/summarize",(req,res)=>{
+  res.status(404).send("YET TO IMPLEMENT")
+})
+
+
+app.post("/caption",(req,res)=>{
+  res.status(404).send("YET TO IMPLEMENT")
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
