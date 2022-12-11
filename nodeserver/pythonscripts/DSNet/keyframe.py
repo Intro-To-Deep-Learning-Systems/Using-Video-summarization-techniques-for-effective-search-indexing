6af33e1610790771fd34d0e60abe1f6d21de3b53
s-suryakiran <suryakiranbdsk@gmail.com>
2022-12-11 00:13:50 -0500

    editted keyframe.py

--- nodeserver/pythonscripts/DSNet/keyframe.py
+++ nodeserver/pythonscripts/DSNet/keyframe.py
@@ -1,15 +1,12 @@
 from Katna.video import Video
 from Katna.writer import KeyFrameDiskWriter
 import os
-if __name__ == "__main__":
+def extract_keyframes(video_file_path):
     vd = Video()
-    no_of_frames_to_returned = 12
+    no_of_frames_to_returned = 10
 
     # initialize diskwriter to save data at desired location
-    diskwriter = KeyFrameDiskWriter(location="selectedframes")
-
-    # Video file path
-    video_file_path = os.path.join("..","..", "uploaded", "files", "image1670653463040.mp4")
+    diskwriter = KeyFrameDiskWriter(location="./nodeserver/uploaded/keyframes")
 
     print(f"Input video file path = {video_file_path}")
 
@@ -18,3 +15,17 @@ if __name__ == "__main__":
         no_of_frames=no_of_frames_to_returned, file_path=video_file_path,
         writer=diskwriter
     )
+
+    file_name = video_file_path.split("/")[-1]
+    file_name = file_name.split(".")[0]
+    file_names = []
+    for i in range(no_of_frames_to_returned):
+        file_names.append("./nodeserver/uploaded/keyframes/"+file_name+"_"+str(i)+".jpeg")
+
+    return file_names
+
+if __name__ == '__main__':
+    video_file_path = os.path.join("/Users/suryakiran/Downloads/using-video-summarization-techniques-for-effective-search-indexing/nodeserver/uploaded/files/image1670653463040.mp4")
+    a = extract_keyframes(video_file_path)
+    print(a)
+
