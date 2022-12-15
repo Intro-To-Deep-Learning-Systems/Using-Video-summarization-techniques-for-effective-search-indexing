1fa3cda7e472af1c558ac42eca005aa47b226fa6
Ashwin Kumar Udayakumar <ashwinkumar14102000@gmail.com>
2022-12-15 01:09:45 -0500

    added testing

--- nodeserver/pythonscripts/server.py
+++ nodeserver/pythonscripts/server.py
@@ -85,16 +85,16 @@ def dynamicsummary():
 
     '''
     #TEST MODE
-    img_list=[os.path.abspath('./nodeserver/pythonscripts/test/summaryof'),
-                os.path.abspath('./nodeserver/pythonscripts/test/summaryof'),
-                os.path.abspath('./nodeserver/pythonscripts/test/summaryof'),
-                os.path.abspath('./nodeserver/pythonscripts/test/summaryof'),
-                os.path.abspath('./nodeserver/pythonscripts/test/summaryof'),
-                os.path.abspath('./nodeserver/pythonscripts/test/summaryof'),
-                os.path.abspath('./nodeserver/pythonscripts/test/summaryof'),
-                os.path.abspath('./nodeserver/pythonscripts/test/summaryof'),
-                os.path.abspath('./nodeserver/pythonscripts/test/summaryof'),
-                os.path.abspath('./nodeserver/pythonscripts/test/summaryof'),]
+    img_list=[os.path.abspath('./nodeserver/pythonscripts/test/5155093236_01067e4f6e_z.jpg'),
+                os.path.abspath('./nodeserver/pythonscripts/test/2633528304_369cf89ce1_z.jpg'),
+                os.path.abspath('./nodeserver/pythonscripts/test/9874637964_ce338575f0_z.jpg'),
+                os.path.abspath('./nodeserver/pythonscripts/test/1176519574_b4d569ecbe_z.jpg'),
+                os.path.abspath('./nodeserver/pythonscripts/test/5093961450_d8c840d0d2_z.jpg'),
+                os.path.abspath('./nodeserver/pythonscripts/test/9372536208_b97bc7eeff_z.jpg'),
+                os.path.abspath('./nodeserver/pythonscripts/test/9419081044_7cebf902ec_z.jpg'),
+                os.path.abspath('./nodeserver/pythonscripts/test/9743494618_f0faea1cdc_z.jpg'),
+                os.path.abspath('./nodeserver/pythonscripts/test/8700354838_884967117d_z.jpg'),
+                os.path.abspath('./nodeserver/pythonscripts/test/9362545580_34d972390d_z.jpg')]
 
     '''
     MODE="CLIPCLAP" #EXPANSIONNET or CLIPCLAP
